135800a626709870da66d7e6ef7c23117d5c5498
Jurasia <jurasiabritton@gmail.com>
2022-12-17 15:38:26 -0500

    commit tree 4ed83c5405606dd676a8f82c8a10afc62fd74181

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+const destructivelyAppendCa = cats.push("Ralph")
