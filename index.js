03046b608ec7c5ed9785e2b0e1e11e558aaa73d1
Jurasia <jurasiabritton@gmail.com>
2023-01-10 12:52:31 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    return cats.push('Ralph')
+
+}
+function destructivelyPrependCat() {
+    return cats.unshift('Bob')
+}
+function destructivelyRemoveLastCat () {
+    return cats.pop('Ralph')
+}
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift(name)
+}
+function appendCat(name) {
+    const copyOfCats = [...cats];
+    copyOfCats.push(name)
+    return copyOfCats
+
+}
+function prependCat(name) {
+    const copyOfCats = [...cats];
+    copyOfCats.unshift(name)
+    return copyOfCats
+
+}
+
+function removeLastCat(name) {
+    const copyOfCats = [...cats];
+    copyOfCats.pop(name)
+    return copyOfCats
+
+}
+
+function removeFirstCat(name) {
+    const copyOfCats = [...cats];
+    copyOfCats.shift(name)
+    return copyOfCats
+
+}
